e871ff119b0751d21d506a46c92afee7ba56d8cb
LY <LY@UPM7IZXTGKKX82X>
2015-09-07 15:05:25 +0800

    修改主页

--- app/src/main/java/net/xinhuamm/dome/MainActivity.java
+++ app/src/main/java/net/xinhuamm/dome/MainActivity.java
@@ -22,5 +22,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         assignViews();
+        text1.setText("============================>");
     }
 }
